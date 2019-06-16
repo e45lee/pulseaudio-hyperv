094b5b81d9db813485d743b2a60e0ad92aaf138e
Russell Treleaven <rtreleaven@bunnykick.ca>
2019-06-16 14:04:06 -0400

    add license and increment version number

--- scripts/pa_info.sh
+++ scripts/pa_info.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-script_version=0.0.0
-changelog="http://some_url"
+
+# This file is part of PulseAudio.
+#
+# Copyright 2019 Russell Treleaven <rtreleaven@bunnykick.ca>
+#
+# PulseAudio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PulseAudio is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with PulseAudio; if not, see <http://www.gnu.org/licenses/>.
+
+script_version=0.0.1
+
 export LC_ALL=C
 header='________________'
 function cards_get {
