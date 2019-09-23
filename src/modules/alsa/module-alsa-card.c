CodeLinaro-mirror/le_pulseaudio

sink, source: Replace avoid-resampling with avoid-processing
This generalises the avoid-resampling concept (don't resample for any
rate above the default/alternate sample rate) to include channel count
and sample format as well. The rationale for this is that users who wish
to send out their data untouched by the resampler are likely to desire
the same on other fronts as well.

In addition to this, there are opportunities for certain hardware (such
as systems with a DSP connected to a codec) to offload processing to the
DSP (providing potential cost savings).

Finally, this also enables modules that might be able to perform
transformations of (ANY -> sink format), and this allows us to implement
such transformations.

src/modules/alsa/module-alsa-card.c
@@ -68,7 +68,7 @@ PA_MODULE_USAGE(
         "profile_set=<profile set configuration file> "
         "paths_dir=<directory containing the path configuration files> "
         "use_ucm=<load use case manager> "
-        "avoid_resampling=<use stream original sample rate if possible?> "
+        "avoid_processing=<use stream original sample rate if possible?> "
 );
 
 static const char* const valid_modargs[] = {
@@ -96,7 +96,7 @@ static const char* const valid_modargs[] = {
     "profile_set",
     "paths_dir",
     "use_ucm",
-    "avoid_resampling",
+    "avoid_processing",
     NULL
 };
 

e45lee/pulseaudio-hyperv

src/modules/alsa/module-alsa-card.c
@@ -68,7 +68,7 @@ PA_MODULE_USAGE(
         "profile_set=<profile set configuration file> "
         "paths_dir=<directory containing the path configuration files> "
         "use_ucm=<load use case manager> "
-        "avoid_resampling=<use stream original sample rate if possible?> "
+        "avoid_processing=<use stream original sample spec if possible?> "
         "control=<name of mixer control> "
 );
 
@@ -97,7 +97,7 @@ static const char* const valid_modargs[] = {
     "profile_set",
     "paths_dir",
     "use_ucm",
-    "avoid_resampling",
+    "avoid_processing",
     "control",
     NULL
 };